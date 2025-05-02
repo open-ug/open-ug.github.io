0b75f2e85cdeced1292efe94b88213183ee721af
jim-junior <jimjunior854@gmail.com>
2025-05-02 12:55:09 +0300

    Add icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,21 @@ export default async function RootLayout({
       <Head
       // ... Your additional head options
       >
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Open UG" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <Layout
