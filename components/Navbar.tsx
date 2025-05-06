3403ef9d4d7d3577e6e6f3868f9c9cadce88657c
jim-junior <jimjunior854@gmail.com>
2025-05-06 19:01:38 +0300

    Fix navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-gray-100 text-black shadow-lg z-10">
+    <nav className="bg-gray-100 text-black shadow-lg sticky right-0 left-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo and site name */}
