b11d2f18579253bc7bf8a94050abfe86d9d35d2c
jim-junior <jimjunior854@gmail.com>
2025-12-30 00:35:50 +0300

    refactor: remove Nextra configuration and simplify Next.js export settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
-import nextra from 'nextra'
 
-const withNextra = nextra({
-  // ... Other Nextra config options
-})
 
-// You can include other Next.js configuration options here, in addition to Nextra settings:
-export default withNextra({
+
+
+export default {
   output: "export"
-})
+}
