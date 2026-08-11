152d3cb9acabb42151399a7192be63955921a17a
jim-junior <jimjunior854@gmail.com>
2026-08-11 18:38:57 +0300

    chore: add pnpm workspace configuration to allow builds for sharp and unrs-resolver
    Signed-off-by: jim-junior <jimjunior854@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 import AboutSection from "@/app/about";
 import Hero from "@/app/hero";
-import React from "react";
-import RoadmapSection from "./conveyotstack";
 import Metrics from "./metrics";
-import RecentPapers from "./papers";
 import Community from "./community";
 import { CTASection } from "./cta";
-import PartnersSection from "./partners";
 import Mesh from "./mesh";
 
 const page = () => {
@@ -17,10 +13,7 @@ const page = () => {
       <Mesh />
       <AboutSection />
       <Metrics />
-      <RecentPapers />
-      <RoadmapSection />
       <Community />
-      <PartnersSection />
       <CTASection />
     </>
   );
